d63f433114e16fe6912a1b46d99dcd19fb9c81bc
mourao-dev <104600647+mourao-dev@users.noreply.github.com>
2023-11-19 23:18:08 -0300

    Add files via upload

---
+++ recursao.c
@@ -0,0 +1,25 @@
+﻿# include <stdio.h>
+
+int fatorial(int n)
+{
+	if (n == 0){return 1;}
+	else {return n*fatorial(n-1);}	
+}
+
+int potencia (int base, int expoente)
+
+{
+	if (expoente == 0){return 1;}
+	return base * potencia(base, expoente-1);
+		
+}
+
+
+int main (void)
+{
+	printf("Fatorial: %d\n", fatorial(5));
+	printf("Potenciacao: %d\n", potencia(2,10));
+
+
+	return 0;
+}
